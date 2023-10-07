1fdc70f3326ed85bf5c765bfbdad6f7dce9bb3fc
GUPTA-DIKSHITA <dikshitagupta@outlook.com>
2023-10-07 23:42:31 +0530

    GENERATE TRY1(BACKEND)

---
+++ exam hall try1/maintry.py
@@ -0,0 +1,53 @@
+import pandas as pd
+df1 = pd.read_excel('2nd list.xlsx')
+df2 = pd.read_excel('3rd list.xlsx')
+roll_no_list1 = df1['URN'].tolist()
+roll_no_list2 = df2['URN'].tolist()
+
+max_row = int(input("Enter the maximum number of rows: "))
+max_col = int(input("Enter the maximum number of columns: "))
+max_capacity = int(input("Enter the maximum capacity of a room: "))
+
+room = []
+room_count = 0
+room_no = 1
+
+filename = f'room_{room_no}.xlsx'
+writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+workbook = writer.book
+worksheet = workbook.add_worksheet()
+row = 0
+col = 0
+
+for i in range(max_row):
+    for j in range(max_col):
+        if room_count == max_capacity:
+            room_count = 0
+            room_no += 1
+            filename = f'room_{room_no}.xlsx'
+            writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+            workbook = writer.book
+            worksheet = workbook.add_worksheet()
+            row = 0
+            col = 0
+        if len(room) == j:
+            room.append([])
+        if i % 2 == 0:
+            roll_no = roll_no_list1[i*max_col+j]
+        else:
+            roll_no = roll_no_list2[i*max_col+j]
+        room[j].append(roll_no)
+        room_count += 1
+        worksheet.write(j, i, roll_no)
+        if room_count == max_capacity:
+            writer._save()
+            worksheet = workbook.add_worksheet()
+            row = 0
+            col = 0
+            room = []
+            room_no += 1
+            filename = f'room_{room_no}.xlsx'
+            writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+            workbook = writer.book
+
+writer._save()
