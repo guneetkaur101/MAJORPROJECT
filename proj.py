e482c87b47aa2eafa33f971e8e7b6ef421b1c4d5
guneetkaur101 <guneetsidhu40@gmail.com>
2023-10-11 23:50:14 +0530

    added upload module

--- proj.py
+++ proj.py
@@ -2,6 +2,7 @@ from flask import Flask , render_template , request, redirect, url_for, flash
 import sqlite3
 import csv
 import os
+import pandas as pd
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -36,64 +37,43 @@ def admin():
 		data = cursor.fetchall()
 	return render_template("admin.html",data=data)
 @app.route("/addroom",methods=['GET','POST'])
+
 def addroom():
-	data=None
-	error = None
-	if request.method == 'POST':
-		room_no = request.form['room_no']
-		row = request.form['row']
-		col = request.form['col']
-		seat = request.form['seat']
-		myconn = sqlite3.connect("room_details.db")
-		if (int(seat) <= int(row) * int(col)):
-			with myconn:
-				cursor = myconn.cursor()
-				cursor.execute("CREATE TABLE IF NOT EXISTS room(room_no integer(10),col integer(10),row integer(10),seat integer(10))")
-				temp_no = cursor.execute("SELECT room_no from room where room_no=?",[room_no])
-				temp_no = cursor.fetchone()	
-			if temp_no is None:
-				with myconn:
-					cursor = myconn.cursor()
-					cursor.execute("CREATE TABLE IF NOT EXISTS room(room_no integer(10),col integer(10),row integer(10),seat integer(10))")
-					cursor.execute("INSERT INTO room VALUES(?,?,?,?)",[room_no,col,row,seat]) 
-					error = room_no + " is added"
-			else:
-				error = room_no + " is already exist"
-		else:
-			error = "Invalid number of seat" 
-	return render_template("addroom.html",error = error,data=data)
-@app.route("/Upload",methods=['GET','POST'])
-def upload():  
     error = None
-    uploaded_files = []
+    capacity = None  # Initialize capacity variable
 
     if request.method == 'POST':
-        # Handle file uploads for 2nd, 3rd, and 4th years
-        for year in ['2nd', '3rd', '4th']:
-            csv_file = request.files.get(f'{year}_year_csv')
-            if csv_file:
-                filename = f'{year}_year.csv'
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                csv_file.save(file_path)
-                uploaded_files.append(filename)
+        room_no = request.form['room_no']
 
-         # If no files were uploaded, list all files in the upload directory
-        if not uploaded_files:
-           uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
+        # Calculate the row and column capacities
+        try:
+            row = int(request.form['row'])
+            col = int(request.form['col'])
+            seat = row * col
 
-        # Modify the response message
-        if uploaded_files:
-			
-            error = "CSV files uploaded successfully."
+            # Set the capacity variable to the calculated value
+            capacity = seat
+        except ValueError:
+            return "Invalid input for row or column."
+
+        myconn = sqlite3.connect("room_details.db")
+
+        with myconn:
+            cursor = myconn.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS room(room_no INTEGER PRIMARY KEY, col INTEGER, row INTEGER, seat INTEGER)")
+            temp_no = cursor.execute("SELECT room_no from room where room_no=?", [room_no])
+            temp_no = cursor.fetchone()
+
+        if temp_no is None:
+            with myconn:
+                cursor = myconn.cursor()
+                cursor.execute("INSERT INTO room(room_no, col, row, seat) VALUES(?,?,?,?)", [room_no, col, row, seat])
+                error = f"Room {room_no} is added with {col} columns and {row} rows, total {seat} seats."
         else:
-            error = "Please upload CSV files."
-	
+            error = f"Room {room_no} is already exist."
 
-    return render_template("upload.html", error=error, uploaded_files=uploaded_files)
-
-   
-
-
+    return render_template("addroom.html", error=error, capacity=capacity)  # Pass capacity to the HTML template
+    
 def show():
 	data=None
 	filename = None
@@ -109,6 +89,7 @@ def show():
 		data= data.to_html()
 		filename = '/static/execl/'+room_no+'.xlsx'
 	return render_template("show_result.html",data=data,room_no=temp_no,filename=filename)
+
 @app.route('/delete/<id>')
 def delete(id):
 	myconn = sqlite3.connect("room_details.db")
@@ -117,37 +98,86 @@ def delete(id):
 		cursor.execute("CREATE TABLE IF NOT EXISTS room(room_no integer(10),col integer(10),row integer(10),seat integer(10))")
 		cursor.execute("DELETE FROM room WHERE room_no=?",[id])
 	return  redirect(url_for('admin'))
+
 @app.route('/edit/<id>',methods=['GET','POST'])
 def edit(id):
-	if request.method == 'POST':
-		room_no = request.form['room_no']
-		row = request.form['row']
-		col = request.form['col']
-		seat = request.form['seat']
-		myconn = sqlite3.connect("room_details.db")
-		if (int(seat) <= int(row) * int(col)):
-			with myconn:
-				cursor = myconn.cursor()
-				cursor.execute("CREATE TABLE IF NOT EXISTS room(room_no integer(10),col integer(10),row integer(10),seat integer(10))")
-				cursor.execute("INSERT INTO room VALUES(?,?,?,?)",[room_no,col,row,seat]) 
-				error = room_no + " is added"
-		else:
-			error = "Invalid number of seat" 
-	error = None
-	myconn = sqlite3.connect("room_details.db")
-	with myconn:
-		cursor = myconn.cursor()
-		data = cursor.execute("SELECT * FROM room WHERE room_no = ?",[id])
-		data = cursor.fetchmany()
-	room_no = data[0][0]
-	col = 	data[0][1]
-	row = data[0][2]
-	seat = data[0][3]
-	return render_template("addroom.html",error = error,room = room_no,col = col, row = row, seat = seat)
+    message = None  # Initialize the message variable
+    if request.method == 'POST':
+        room_no = request.form['room_no']
+        col = request.form['col']
+        row = request.form['row']
+        seat = request.form['seat']
+        
+        myconn = sqlite3.connect("room_details.db")
+        with myconn:
+            cursor = myconn.cursor()
+            cursor.execute("UPDATE room SET room_no=?, col=?, row=?, seat=? WHERE room_no=?", (room_no, col, row, seat, id))
+            myconn.commit()
+            message = "Room details have been updated successfully."  # Set the message
+            return redirect('/admin')  # Redirect to the admin page
 
+    myconn = sqlite3.connect("room_details.db")
+    with myconn:
+        cursor = myconn.cursor()
+        cursor.execute("SELECT * FROM room WHERE room_no = ?", [id])
+        data = cursor.fetchone()
+    
+    if data:
+        room_no = data[0]
+        col = data[1]
+        row = data[2]
+        seat = data[3]
+    else:
+        room_no = None
+        col = None
+        row = None
+        seat = None
 
+    return render_template("editroom.html", room_no=room_no, col=col, row=row, seat=seat, message=message)
 
+@app.route('/upload_form', methods=['GET', 'POST'])
+def upload_form():
+    total_strengths = {}  # Initialize as an empty dictionary
+    # Define the list of available years
+    available_years = ['2nd', '3rd', '4th']
+    if request.method == 'POST':
+        print("Processing started") 
+        # Get the selected years from checkboxes
+        selected_years = request.form.getlist('select_year')
+        # # Check if at least one year is selected (already alert box)
+        # if not selected_years:
+        #     error = "Please select at least one year"
+        #     return render_template('upload_form.html', total_strengths=total_strengths, error=error)
+        upload_errors = []
 
+        for year in available_years:
+            if year in selected_years:
+                csv_file = request.files.get(f'{year}_year_csv')
+
+                if not csv_file:
+                    upload_errors.append(f"Please upload a file for {year} year.")
+                    continue  # Skip this year if the file is missing
+
+                filename = f'{year}_year.csv'
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                csv_file.save(file_path)
+                print("File saved") 
+                # df = pd.read_csv(file_path)
+
+                # # Exclude the top two rows (assuming they are headers)
+                # df = df.iloc[1:]
+                
+                df = pd.read_csv(file_path, skiprows=1)
+                # Calculate the number of entries (rows) in the CSV
+                num_entries = len(df)
+                total_strengths[year] = num_entries
+
+        if upload_errors:
+            return render_template('upload_form.html', total_strengths=total_strengths, upload_errors=upload_errors)
+        print("Processing completed")
+        return render_template('upload_form.html', total_strengths=total_strengths)
+
+    return render_template('upload_form.html', total_strengths=total_strengths)
 
 if __name__ == "__main__":
     app.run()
