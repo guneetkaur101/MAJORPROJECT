4678675a2ccad5d9204e1dfae4e6b2f99e6cd210
guneetkaur101 <guneetsidhu40@gmail.com>
2023-10-14 16:51:00 +0530

    added genrated

--- proj.py
+++ proj.py
@@ -99,41 +99,78 @@ def delete(id):
 		cursor.execute("DELETE FROM room WHERE room_no=?",[id])
 	return  redirect(url_for('admin'))
 
-@app.route('/edit/<id>',methods=['GET','POST'])
-def edit(id):
-    message = None  # Initialize the message variable
+@app.route('/edit/<room_no>', methods=['GET', 'POST'])
+def edit(room_no):
+    error = None
     if request.method == 'POST':
-        room_no = request.form['room_no']
-        col = request.form['col']
-        row = request.form['row']
-        seat = request.form['seat']
-        
+        # Retrieve the updated details from the form
+        col = request.form['columns']
+        rows_per_columns = [request.form[f'row_c{i}'] for i in range(1, 7)]
+        print(rows_per_columns)
+        # Calculate the total number of seats
+        total_seats = sum([int(rows) for rows in rows_per_columns])
+        print(total_seats)
         myconn = sqlite3.connect("room_details.db")
         with myconn:
-            cursor = myconn.cursor()
-            cursor.execute("UPDATE room SET room_no=?, col=?, row=?, seat=? WHERE room_no=?", (room_no, col, row, seat, id))
-            myconn.commit()
-            message = "Room details have been updated successfully."  # Set the message
-            return redirect('/admin')  # Redirect to the admin page
-
+           cursor = myconn.cursor()
+        # Update the room details in the database
+           cursor.execute("UPDATE room SET col=?, row_c1=?, row_c2=?, row_c3=?, row_c4=?, row_c5=?, row_c6=?, u_row_c1=?, u_row_c2=?, u_row_c3=?, u_row_c4=?, u_row_c5=?, u_row_c6=?, seat=? WHERE room_no=?", 
+                       [col] + rows_per_columns + rows_per_columns + [total_seats, room_no])
+           myconn.commit()
+           error = "Room details have been updated successfully."
+           return redirect('/admin')  # Redirect to the admin page
+    
     myconn = sqlite3.connect("room_details.db")
     with myconn:
         cursor = myconn.cursor()
-        cursor.execute("SELECT * FROM room WHERE room_no = ?", [id])
-        data = cursor.fetchone()
-    
-    if data:
-        room_no = data[0]
-        col = data[1]
-        row = data[2]
-        seat = data[3]
-    else:
-        room_no = None
-        col = None
-        row = None
-        seat = None
+    # Fetch the current room details for rendering the edit page
+        cursor.execute("SELECT * FROM room WHERE room_no = ?", [room_no])
+        room_details = cursor.fetchone()
 
-    return render_template("editroom.html", room_no=room_no, col=col, row=row, seat=seat, message=message)
+    if room_details:
+        col = room_details[1]
+        row_details = room_details[2:15]
+    else:
+        col = None
+        row_details = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    return render_template("editroom.html", room_no=room_no, col=col, row_details=row_details, error=error)
+
+# @app.route('/edit/<id>',methods=['GET','POST'])
+# def edit(id):
+#     message = None  # Initialize the message variable
+#     if request.method == 'POST':
+#         room_no = request.form['room_no']
+#         col = request.form['col']
+#         row = request.form['row']
+#         seat = request.form['seat']
+        
+#         myconn = sqlite3.connect("room_details.db")
+#         with myconn:
+#             cursor = myconn.cursor()
+#             cursor.execute("UPDATE room SET room_no=?, col=?, row=?, seat=? WHERE room_no=?", (room_no, col, row, seat, id))
+#             myconn.commit()
+#             message = "Room details have been updated successfully."  # Set the message
+#             return redirect('/admin')  # Redirect to the admin page
+
+#     myconn = sqlite3.connect("room_details.db")
+#     with myconn:
+#         cursor = myconn.cursor()
+#         cursor.execute("SELECT * FROM room WHERE room_no = ?", [id])
+#         data = cursor.fetchone()
+    
+#     if data:
+#         room_no = data[0]
+#         col = data[1]
+#         row = data[2]
+#         seat = data[3]
+#     else:
+#         room_no = None
+#         col = None
+#         row = None
+#         seat = None
+
+#     return render_template("editroom.html", room_no=room_no, col=col, row=row, seat=seat, message=message)
 @app.route('/upload_form', methods=['GET', 'POST'])
 def upload_form():
     total_strengths = {}  # Initialize as an empty dictionary
@@ -178,6 +215,79 @@ def upload_form():
 
     return render_template('upload_form.html', total_strengths=total_strengths)
 
+@app.route("/generate", methods=['GET', 'POST'])
+def generate():
+    myconn = sqlite3.connect("room_details.db")
+    with myconn:
+        cursor = myconn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS room(room_no integer(10),col integer(10),row_c1 integer(10),row_c2 integer(10),row_c3 integer(10),row_c4 integer(10),row_c5 integer(10),row_c6 integer(10),u_row_c1 integer(10),u_row_c2 integer(10),u_row_c3 integer(10),u_row_c4 integer(10),u_row_c5 integer(10),u_row_c6 integer(10),seat integer(10))")
 
+    if request.method == 'POST':
+        selected_rooms = request.form.getlist('selected_rooms')
+        print(selected_rooms)
+        # Process the selected rooms here as needed.
+        # You can perform actions like generating a report, storing selections, etc.
+
+    # Fetch data for the table
+    cursor.execute("SELECT * FROM room")
+    data = cursor.fetchall()
+
+    return render_template("generate.html", data=data)
+# @app.route("/edit-usable-rows/<room_no>", methods=['GET','POST'])
+# def edit_usable_rows(room_no):
+    
+#     usable_row_c1 = request.form.get('usable_row_c1')
+#     usable_row_c2 = request.form.get('usable_row_c2')
+#     usable_row_c3 = request.form.get('usable_row_c3')
+#     usable_row_c4 = request.form.get('usable_row_c4')
+#     usable_row_c5 = request.form.get('usable_row_c5')
+#     usable_row_c6 = request.form.get('usable_row_c6')
+    
+#     myconn = sqlite3.connect("room_details.db")
+#     with myconn:
+#         cursor = myconn.cursor()
+      
+#         # Update the database with the new usable rows data
+#         cursor.execute("UPDATE room SET u_row_c1=?, u_row_c2=?, u_row_c3=?, u_row_c4=?, u_row_c5=?, u_row_c6=? WHERE room_no=?", (usable_row_c1, usable_row_c2, usable_row_c3, usable_row_c4, usable_row_c5, usable_row_c6, room_no))
+        
+#     # Redirect back to the page after the update.
+#     return redirect("/generate") 
+def get_room_data(room_no):
+    # This function retrieves the data for the specified room from the database.
+    myconn = sqlite3.connect("room_details.db")
+    cursor = myconn.cursor()
+    cursor.execute("SELECT * FROM room WHERE room_no=?", (room_no,))
+    room_data = cursor.fetchone()
+    myconn.close()
+    return room_data
+@app.route("/edit-usable-rows/<room_no>", methods=['GET', 'POST'])
+def edit_usable_rows(room_no):
+    if request.method == 'POST':
+        # Get the submitted form data and update the database
+        usable_row_c1 = request.form.get('usable_row_c1')
+        usable_row_c2 = request.form.get('usable_row_c2')
+        usable_row_c3 = request.form.get('usable_row_c3')
+        usable_row_c4 = request.form.get('usable_row_c4')
+        usable_row_c5 = request.form.get('usable_row_c5')
+        usable_row_c6 = request.form.get('usable_row_c6')
+
+        myconn = sqlite3.connect("room_details.db")
+        cursor = myconn.cursor()
+
+        # Update the database with the new usable rows data
+        cursor.execute(
+            "UPDATE room SET u_row_c1=?, u_row_c2=?, u_row_c3=?, u_row_c4=?, u_row_c5=?, u_row_c6=? WHERE room_no=?",
+            (usable_row_c1, usable_row_c2, usable_row_c3, usable_row_c4, usable_row_c5, usable_row_c6, room_no)
+        )
+        myconn.commit()
+        myconn.close()
+
+        # Redirect back to the "generate" page after the update
+        return redirect("/generate")
+
+    # In the GET request, you can fetch the room data to display in the form
+    room_data = get_room_data(room_no)
+
+    return render_template("edit_usable_rows.html", room_data=room_data)
 if __name__ == "__main__":
     app.run()
