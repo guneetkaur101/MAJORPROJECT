d5a358dd8585a3781db9ebd1c8fa252c667bf70f
Gursimar Singh <sethigursimar@gmail.com>
2023-11-09 18:10:12 +0530

    finalised for normal and main for defined functions

--- exam hall try1/main.py
+++ exam hall try1/main.py
@@ -31,110 +31,109 @@ def get_selected_subjects(subject_headings):
             print("Invalid choice. Please enter a valid number.")
     return selected_subjects
 
-# Load subject data
-df1, df2, df3 = load_subject_data()
-subject_headings = list(df1.columns) + list(df2.columns) + list(df3.columns)
+def create_subject_data(df1, df2, df3, selected_subjects):
+    # Create a dictionary to store the subject data and the current roll number position
+    subject_data = {subject: df1[subject] if subject in df1.columns else
+                   df2[subject] if subject in df2.columns else
+                   df3[subject]
+                   for subject in selected_subjects}
+    return subject_data
 
-# Get selected subjects from the user
-selected_subjects = get_selected_subjects(subject_headings)
+def create_subject_positions(selected_subjects):
+    subject_positions = {subject: 0 for subject in selected_subjects}
+    return subject_positions
 
-# Create a deque with a maximum length of 3 for rotating subjects
-rotating_subjects = deque(selected_subjects[:3])
+def connect_to_database():
+    # Connect to SQLite database with room details
+    myconn = sqlite3.connect("room_details.db")
+    return myconn
 
-# Create a list to store subjects that are waiting
-waiting_subjects = selected_subjects[3:]
+def get_room_details(myconn):
+    with myconn:
+        cursor = myconn.cursor()
+        cursor.execute("SELECT room_no, u_row_c1, u_row_c2, u_row_c3, u_row_c4, u_row_c5, u_row_c6 from room")
+        rows = cursor.fetchall()
+    return rows
 
-# Sort the selected subjects
-selected_subjects.sort()
-
-# Create a dictionary to store the subject data and the current roll number position
-subject_data = {subject: df1[subject] if subject in df1.columns else
-               df2[subject] if subject in df2.columns else
-               df3[subject]
-               for subject in selected_subjects}
-
-subject_positions = {subject: 0 for subject in selected_subjects}
-
-# Connect to SQLite database with room details
-myconn = sqlite3.connect("room_details.db")
-with myconn:
-    cursor = myconn.cursor()
-    cursor.execute("SELECT room_no, u_row_c1, u_row_c2, u_row_c3, u_row_c4, u_row_c5, u_row_c6 from room")
-    rows = cursor.fetchall()
-
+def create_excel_writer():
     # Create Excel writer
     writer = pd.ExcelWriter(FILENAME, engine='xlsxwriter')
+    return writer
+
+def create_workbook(writer):
     workbook = writer.book
+    return workbook
 
-    for row in rows:
-        room_no = row[0]
-        room_name = str(room_no)
-        max_rows = row[1:]  # Extract maximum row counts for each column
+def create_room_schedule():
+    # Initialize a schedule list for the room
+    room_schedule = [[] for _ in range(MAX_COLS)]
+    return room_schedule
 
-        # Initialize a schedule list for the room
-        room_schedule = [[] for _ in range(MAX_COLS)]
+def check_rotating_subjects(rotating_subjects):
+    # Check if rotating_subjects is empty, terminate the program
+    if not rotating_subjects:
+        return False
+    return True
 
-        for current_col in range(MAX_COLS):
-            # Check if rotating_subjects is empty, terminate the program
-            subject_heading_added = False
-            if not rotating_subjects:
+def add_subject_to_schedule(rotating_subjects, room_schedule, current_col):
+    if len(rotating_subjects) != 1: 
+        room_schedule[current_col].append(rotating_subjects[0]) 
+    return room_schedule
+
+def allocate_students_to_room(rotating_subjects, room_schedule, current_col, max_rows, subject_positions, subject_data, waiting_subjects):
+    for _ in range(max_rows[current_col]):
+        current_subject = rotating_subjects[0]
+        # Get the current subject from the rotating queue
+        if len(rotating_subjects) == 1:
+            if (current_col - 1) % 2 == 0:
+                # Skip one column allocation on even columns
+                current_col += 1
                 break
-            if len(rotating_subjects) != 1: 
-                    room_schedule[current_col].append(rotating_subjects[0]) 
-                    
-            for _ in range(max_rows[current_col]):
-                current_subject = rotating_subjects[0]
-                 # Get the current subject from the rotating queue
+            elif current_col % 2 == 0:
+                max_rows = list(max_rows)
+                max_rows[current_col] = 0
+                max_rows = tuple(max_rows)
 
-                # Inside the loop where you handle column allocation
-                if len(rotating_subjects) == 1:
-                    
+        if subject_positions[current_subject] < len(subject_data[current_subject]):
+            student = subject_data[current_subject].iloc[subject_positions[current_subject]]
+            room_schedule[current_col].append(student)
+            subject_positions[current_subject] += 1
 
-                    if (current_col - 1) % 2 == 0:
-                        # Skip one column allocation on even columns
-                        current_col += 1
-                        break
-                    elif current_col % 2 == 0:
-                        if not subject_heading_added:
-                            room_schedule[current_col].append(rotating_subjects[0])
-                            subject_heading_added = True
+        # Check if the subject is exhausted and replace it
+        if subject_positions[current_subject] >= len(subject_data[current_subject]):
+            rotating_subjects.popleft()
+            if waiting_subjects:
+                rotating_subjects.append(waiting_subjects.pop(0))
+                rotating_subjects.rotate(+1)
+                
+            elif not waiting_subjects:
+                rotating_subjects.rotate(+1)# Treat the column as filled
+                break
+    return room_schedule, rotating_subjects, subject_positions
 
-                        max_rows = list(max_rows)
-                        max_rows[current_col] = 0
-                        max_rows = tuple(max_rows)
+def rotate_subjects(rotating_subjects):
+    # Rotate the subjects once a column has finished
+    rotating_subjects.rotate(-1)
+    return rotating_subjects
 
-# Continue with the rest of the loop...
+def create_worksheet(workbook, room_name):
+    # Create a worksheet for the room
+    worksheet = workbook.add_worksheet(room_name)
+    return worksheet
 
-                     
+def write_to_worksheet(worksheet, room_name, room_schedule):
+    worksheet.write(1, 1, "Date = " + DATE)
+    worksheet.write(1, 5, "Time = " + TIME)
+    room_name_format = workbook.add_format({'bold': True, 'font_size': 22})
+    worksheet.write(0, 3, room_name, room_name_format)
+    worksheet.set_row(0, 30)  # Set the row height to double
+    # Write the scheduled students to the worksheet
+    for col, students in enumerate(room_schedule):
+        for row, student in enumerate(students):
+            worksheet.write(row+3, col, student)
+    return worksheet
 
-                if subject_positions[current_subject] < len(subject_data[current_subject]):
-                    student = subject_data[current_subject].iloc[subject_positions[current_subject]]
-                    room_schedule[current_col].append(student)
-                    subject_positions[current_subject] += 1
+def save_excel_file(writer):
+    # Save the Excel file
+    writer._save()
 
-                # Check if the subject is exhausted and replace it
-                if subject_positions[current_subject] >= len(subject_data[current_subject]):
-                    rotating_subjects.popleft()
-                    if waiting_subjects:
-                        rotating_subjects.append(waiting_subjects.pop(0))
-                        rotating_subjects.rotate(+1)
-                        
-                    elif not waiting_subjects:
-                        # rotating_subjects.append(current_subject)
-                        rotating_subjects.rotate(+1)# Treat the column as filled
-                        break
-
-            # Rotate the subjects once a column has finished
-            rotating_subjects.rotate(-1)
-            
-
-        # Create a worksheet for the room
-        worksheet = workbook.add_worksheet(room_name)
-
-        # Write the scheduled students to the worksheet
-        for col, students in enumerate(room_schedule):
-            for row, student in enumerate(students):
-                worksheet.write(row, col, student)
-
-# Save the Excel file
-writer._save()
